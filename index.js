cea69e4cc9c22fc36a5ef837eb440b4d371c8264
Edenilton <edeniltonborba.adm@gmail.com>
2019-11-28 11:18:44 +0100

    add index.js

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 const express = require("express");
 const app = express();
-
-const PORT = 3000;
+const path = require("path")
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const tables = [];
+const waitlist = [];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "/tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "/reserve.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables)
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist)
+});
+
+app.post("/api/tables", function(req, res){
+    try {
+        console.log(req.body);
+        var newReserve = req.body || {};
+
+        newReserve.customerName = newReserve.customerName.replace(/\s+/g, "").toLowerCase();
+
+        if (tables.length < 5) {
+            tables.push(newReserve);
+        } else {
+            waitlist.push(newReserve);
+        }
+        res.json(tables);
+    } catch (error) {
+        console.log(error);
+        return res.json(error);
+    }
+});
+
+
 
 
 app.listen(PORT, function() {
